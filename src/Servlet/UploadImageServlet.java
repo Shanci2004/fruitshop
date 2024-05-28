02c30d7a88ceef8d33e0e7ee58f8ae4e66815bee
Shanci2004 <894918234@qq.com>
2024-05-28 22:09:58 +0800

    上传头像接口

---
+++ src/Servlet/UploadImageServlet.java
@@ -0,0 +1,28 @@
+package Servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.File;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "UploadImageServlet", urlPatterns = "/Upload_Image")
+public class UploadImageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //获取头像图片存入Part中
+        Part avatar = request.getPart("avatar");
+        //storePath将头像存入某处
+        String storePath = getServletContext().getRealPath("avatar");
+        new File(storePath).mkdirs();
+        //
+        String filename = System.currentTimeMillis() + avatar.getSubmittedFileName();//保证图片名不重复
+        avatar.write(storePath + "/" + filename);
+
+    }
+}
