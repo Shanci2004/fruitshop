f9d57f1a03d4c95bc0b24e122f6c178d7fa7db02
Shanci2004 <894918234@qq.com>
2024-06-09 16:47:15 +0800

    管理员业务

--- src/Servlet/AdminServlet/AdminFruitModifyServlet.java
+++ src/Servlet/AdminServlet/AdminFruitModifyServlet.java
@@ -6,12 +6,14 @@ import net.sf.json.JSONObject;
 import utiils.FruitImageUtils;
 
 import javax.servlet.*;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet(name = "AdminFruitModifyServlet", urlPatterns = "/Admin_FruitModify")
+@MultipartConfig
 public class AdminFruitModifyServlet extends HttpServlet {
     private FruitService fruitService = new FruitService();
     @Override
